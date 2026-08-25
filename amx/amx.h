271406025ada497cbeb16bb72ecc1697d9b9fd1b
1therealcloud <1therealcloud@gmail.com>
2026-08-25 21:08:02 +0300

    modernize stdint and alloca

--- amx/amx.h
+++ amx/amx.h
@@ -33,64 +33,24 @@
 
 #include <stdlib.h>   /* for size_t */
 #include <limits.h>
+#include <stdint.h>
 
-#if defined HAVE_STDINT_H
-  #include <stdint.h>
-#else
-  #if defined __LCC__ || defined __DMC__ || defined LINUX
-	#if defined HAVE_INTTYPES_H
-	  #include <inttypes.h>
-	#else
-	  #include <stdint.h>
-	#endif
-  #elif !defined __STDC_VERSION__ || __STDC_VERSION__ < 199901L
-	/* The ISO C99 defines the int16_t and int_32t types. If the compiler got
-	 * here, these types are probably undefined.
-	 */
-	#if defined __MACH__
-	  #include <inttypes.h>
-	  typedef unsigned short int  uint16_t;
-	  typedef unsigned long int   uint32_t;
-	#elif defined __FreeBSD__
-	  #include <inttypes.h>
-	#else
-	  typedef short int           int16_t;
-	  typedef unsigned short int  uint16_t;
-	  #if defined SN_TARGET_PS2
-		typedef int               int32_t;
-		typedef unsigned int      uint32_t;
-	  #else
-		typedef long int          int32_t;
-		typedef unsigned long int uint32_t;
-	  #endif
-	  #if defined __WIN32__ || defined _WIN32 || defined WIN32
-		typedef __int64	          int64_t;
-		typedef unsigned __int64  uint64_t;
-		#define HAVE_I64
-	  #elif defined __GNUC__
-		typedef long long         int64_t;
-		typedef unsigned long long uint64_t;
-		#define HAVE_I64
-	  #endif
-	#endif
-  #endif
+#if !defined HAVE_STDINT_H
   #define HAVE_STDINT_H
 #endif
-#if defined _LP64 || defined WIN64 || defined _WIN64
-  #if !defined __64BIT__
-	#define __64BIT__
-  #endif
+#if !defined HAVE_I64
+  #define HAVE_I64
 #endif
 
-#if HAVE_ALLOCA_H
-  #include <alloca.h>
-#elif HAVE_MALLOC_H //alloca could be also defined here
+#if defined _MSC_VER
   #include <malloc.h>
-#endif
-#if defined __WIN32__ || defined _WIN32 || defined WIN32 /* || defined __MSDOS__ */
   #if !defined alloca
 	#define alloca(n)   _alloca(n)
   #endif
+#elif defined __GNUC__ || defined __clang__
+  #include <alloca.h>
+#else
+  #error Unsupported compiler: alloca is required
 #endif
 
 #if !defined arraysize
